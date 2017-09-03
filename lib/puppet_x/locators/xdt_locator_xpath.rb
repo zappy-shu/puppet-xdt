37db86024c98abee28b83191105d90c1b44bce66
Nick Adcock <nick.adcock@bango.com>
2017-09-03 15:18:34 +0100

    adding xpath locator

--- lib/puppet_x/locators/xdt_locator_xpath.rb
+++ lib/puppet_x/locators/xdt_locator_xpath.rb
@@ -4,5 +4,11 @@ require 'nokogiri'
 class XdtLocatorXpath
     def locate(source_node, locator_node, locator_arguments)
         XdtLocatorLogger.log_action('Xpath', source_node, locator_node, locator_arguments)
+        unless locator_arguments.length == 1
+            warn "Xpath locator requires 1 locator argument that is the full xpath"
+            return []
+        end
+
+        return source_node.document.xpath(locator_arguments[0])
     end
 end
--- spec/puppet_x/locators/xdt_locator_xpath_spec.rb
+++ spec/puppet_x/locators/xdt_locator_xpath_spec.rb
@@ -3,5 +3,63 @@ require 'nokogiri'
 
 describe XdtLocatorXpath do
     describe '#locate' do
+        context 'when no locator attributes given' do
+            it 'warns and returns empty array' do
+                source_doc = Nokogiri::XML('<root><a/></root>')
+                locator_doc = Nokogiri::XML('<root/>')
+                source_node = source_doc.xpath('/root/a')[0]
+                locator_node = locator_doc.root
+
+                matching_nodes = XdtLocatorXpath.new.locate(source_node, locator_node, [])
+                expect(matching_nodes.length).to eql(0)
+            end
+        end
+        context 'when more than 1 locator attributes given' do
+            it 'warns and returns empty array' do
+                source_doc = Nokogiri::XML('<root><a/></root>')
+                locator_doc = Nokogiri::XML('<root/>')
+                source_node = source_doc.xpath('/root/a')[0]
+                locator_node = locator_doc.root
+
+                matching_nodes = XdtLocatorXpath.new.locate(source_node, locator_node, ['1', '2'])
+                expect(matching_nodes.length).to eql(0)
+            end
+        end
+        context 'when no node matches xpath' do
+            it 'returns empty array' do
+                source_doc = Nokogiri::XML('<root><a/></root>')
+                locator_doc = Nokogiri::XML('<root/>')
+                source_node = source_doc.xpath('/root/a')[0]
+                locator_node = locator_doc.root
+
+                matching_nodes = XdtLocatorXpath.new.locate(source_node, locator_node, ['//root/b'])
+                expect(matching_nodes.length).to eql(0)
+            end
+        end
+        context 'when 1 node matches xpath' do
+            it 'return matching node' do
+                source_doc = Nokogiri::XML('<root><a/></root>')
+                locator_doc = Nokogiri::XML('<root/>')
+                source_node = source_doc.xpath('/root/a')[0]
+                locator_node = locator_doc.root
+
+                matching_nodes = XdtLocatorXpath.new.locate(source_node, locator_node, ['//root/a'])
+                expect(matching_nodes.length).to eql(1)
+                expect(matching_nodes[0]).to eql(source_node)
+            end
+        end
+        context 'when 2 nodes matches xpath' do
+            it 'return both matching node' do
+                source_doc = Nokogiri::XML('<root><a/><a/></root>')
+                locator_doc = Nokogiri::XML('<root/>')
+                source_nodes = source_doc.xpath('/root/a')
+                locator_node = locator_doc.root
+
+                matching_nodes = XdtLocatorXpath.new.locate(source_nodes[0], locator_node, ['//root/a'])
+                expect(matching_nodes.length).to eql(2)
+                expect(matching_nodes[0]).to eql(source_nodes[0])
+                expect(matching_nodes[1]).to eql(source_nodes[1])
+            end
+        end
     end
 end
