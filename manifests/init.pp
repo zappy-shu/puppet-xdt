23580cd1ac6ead81090104b6d48e1ecfa3c0ba91
Nick Adcock <nick.adcock@bango.com>
2017-09-05 00:46:39 +0100

    removing test files

--- manifests/init.pp
+++
@@ -1,5 +0,0 @@
-xdt_file { 'C:/temp/puppet.config':
-    source_file => 'C:\Development\androidbilling\AndroidBilling\Web.config',
-    transform_file => 'C:\Development\androidbilling\AndroidBilling\Web.staging.config',
-    ensure => present,
-}
